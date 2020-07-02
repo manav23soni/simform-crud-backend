671c0f673dc86f30bccf8ecd42728fc16d49c3a9
manav23soni <sonimanav0007@gmail.com>
2020-07-02 10:53:01 +0530

    Add Validation for add and edit route

--- modules/v1/dynamicRoute/dynamicRouteMiddleware.js
+++ modules/v1/dynamicRoute/dynamicRouteMiddleware.js
@@ -1,9 +1,14 @@
 const l10n = require("jm-ez-l10n");
 const { check } = require("express-validator");
-const utils = require('../../../helper/utils');
+const utils = require("../../../helper/utils");
 const dynamicRouteService = require("./dynamicRouteService");
 const logger = require("../../../helper/logger");
-const { ERROR422, ERROR500, ERROR400, ERROR401 } = require("../../../constants/common");
+const {
+  ERROR422,
+  ERROR500,
+  ERROR400,
+  ERROR401,
+} = require("../../../constants/common");
 
 const middleware = {};
 // Check require parameters
@@ -46,13 +51,25 @@ middleware.checkRouteUrl = async (req, res, next) => {
 // Check duplicate routes
 middleware.checkDuplicateRouteName = async (req, res, next) => {
   try {
-    const { routeName } = req.body  ;
-    const findRouteName = await dynamicRouteService.findRouteName(routeName);
+    const { body: routeDetails } = req;
+    const { id } = req.authUserDetails;
+    const findRouteName = await dynamicRouteService.findRouteName(routeDetails.routeName);
     if (findRouteName) {
-      return res.status(ERROR400.CODE).json({
-        error: req.t("ROUTE_ALREADY_EXIST"),
-        code: ERROR400.CODE,
-      });
+      // Check duplicate for same user
+      if (findRouteName.userId === id && req.method === "POST") {
+        return res.status(ERROR400.CODE).json({
+          error: req.t("ROUTE_ALREADY_EXIST"),
+          code: ERROR400.CODE,
+        });
+        // check duplicate for same user but different routes
+      } else if (findRouteName.userId === id && req.method === "PUT" && findRouteName.id !== routeDetails.id) {
+        return res.status(ERROR400.CODE).json({
+          error: req.t("ROUTE_ALREADY_EXIST"),
+          code: ERROR400.CODE,
+        });
+      } else {
+        return next();
+      }
     } else {
       // Unique Route
       return next();
@@ -68,7 +85,7 @@ middleware.checkDuplicateRouteName = async (req, res, next) => {
 
 // Check parameter validation for requedted route
 middleware.checkValidation = (req, res, next) => {
-  const {body: bodyParameters} = req;
+  const { body: bodyParameters } = req;
   const routeParameters = JSON.parse(req.routeDetails.parameters);
   let requireValidations = [];
   // Here we are comparing DB parameters with Body parameters
@@ -76,7 +93,9 @@ middleware.checkValidation = (req, res, next) => {
     requireValidations = Object.keys(bodyParameters);
     for (let i = 0; i < routeParameters.length; i++) {
       // Check body parameter is found in DB configure parameter
-      let findIndex = requireValidations.findIndex((x) => x === routeParameters[i].field)
+      let findIndex = requireValidations.findIndex(
+        (x) => x === routeParameters[i].field
+      );
       if (findIndex === -1) {
         // Error for require parameter
         return res.status(ERROR422).json({
@@ -94,12 +113,12 @@ middleware.checkValidation = (req, res, next) => {
     });
   }
   return next();
-}
+};
 
 // check user accesss
 middleware.checkRouteUser = async (req, res, next) => {
   try {
-    const {id} = req.authUserDetails;
+    const { id } = req.authUserDetails;
     const routeDetails = await dynamicRouteService.getRouteById(req.params.id);
     if (routeDetails) {
       // Check login user and route access user same or not
@@ -108,23 +127,23 @@ middleware.checkRouteUser = async (req, res, next) => {
       } else {
         // If not than throw permission error
         return res.status(ERROR401.CODE).json({
-          error: req.t('PERMISSION_ERROR'),
+          error: req.t("PERMISSION_ERROR"),
           code: ERROR401.CODE,
         });
       }
     } else {
       return res.status(ERROR401.CODE).json({
-        error: req.t('ERROR_ROUTE_NOT_FOUND'),
+        error: req.t("ERROR_ROUTE_NOT_FOUND"),
         code: ERROR400.CODE,
       });
     }
   } catch (error) {
-    logger.error('Error From checkProductUser() in productMiddleware', error);
+    logger.error("Error From checkProductUser() in productMiddleware", error);
     return res.status(ERROR500.CODE).json({
-      error: req.t('TRY_AGAIN'),
+      error: req.t("TRY_AGAIN"),
       code: ERROR500.CODE,
     });
   }
-}
+};
 
 module.exports = middleware;
